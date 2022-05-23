abd81944e1f6cda79c35dabd140c35e09f5b0408
billy <billyang302@gmail.com>
2022-05-23 12:44:29 -0700

    lunch time 5/23

--- webserver/auth.py
+++ webserver/auth.py
@@ -2,14 +2,14 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
-    return render_template('login.html', text='Work in Progress')
+    return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
     return render_template('logout.html')
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET','POST'])
 def sing_up():
     return render_template('sign_up.html')
