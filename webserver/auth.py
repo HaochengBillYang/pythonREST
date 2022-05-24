f387f3c0467dadd59292ca5ac95f9360d28ae183
billy <billyang302@gmail.com>
2022-05-23 18:02:51 -0700

    framework done

--- webserver/auth.py
+++ webserver/auth.py
@@ -1,15 +1,61 @@
-from flask import Blueprint, render_template
+from gettext import Catalog
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from .  import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+
+
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
-    return render_template('login.html')
+    data = request.form
+    if request.method == 'POST':
+        usr = request.form.get('username')
+        pwd = request.form.get('password')
+        user = User.query.filter_by(username=usr).first()
+        if user:
+            if check_password_hash(user.password, pwd):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for("display.home"))
+            else:
+                flash('Incorrect password', category='error')
+        else:
+            flash('No such user', category='error')
+
+    print(data)
+    return render_template('login.html', user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return render_template('logout.html')
+    logout_user()
+    return redirect(url_for('auth.login'))
+
 
 @auth.route('/sign-up', methods=['GET','POST'])
-def sing_up():
-    return render_template('sign_up.html')
+def sign_up():
+    if request.method == 'POST':
+        usr = request.form.get('username')
+        pwd = request.form.get('password')
+        pwd1 = request.form.get('password1')
+        
+        if pwd != pwd1:
+            flash("check password", category='error')
+        elif pwd == '':
+            flash("null password", category='error')
+        elif User.query.filter_by(username=usr).first():
+            flash("user already exist", category='error')
+        else:
+            new_user = User(username = usr, password = generate_password_hash(pwd, method='sha1'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash("account added", category = 'success')
+            return redirect(url_for("display.home"))
+
+
+    return render_template('sign_up.html', user=current_user)
--- webserver/display.py
+++ webserver/display.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 display = Blueprint('display', __name__)
 
 @display.route('/')
+@login_required
 def home():
-    return render_template('home.html',bool1=True, bool2 = False)
+    return render_template('home.html', user = current_user)
