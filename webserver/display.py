5e1c5bac0d75caa4d57ab0c812103b4373206458
billy <billyang302@gmail.com>
2022-05-27 13:59:15 -0700

    5.27

--- webserver/display.py
+++ webserver/display.py
@@ -28,20 +28,63 @@ def manage():
     url = session['url']
     try:                            # Add loop to continuous get data
             key = op.get_token(url, port)
+            session['all_disks'] = []
             session['clusters'] = []
+            session['disks'] = []
+            session['hosts'] = []
             res1 = op.get_all_clusters(url,port,key)
             session['clusters'] = op.get_all_cluster_id_from_response(res1)
             for i in range(len(session['clusters'])):
                 res2 = op.get_hosts_by_cluster_id(url,port,key,session['clusters'][i])
                 host_temp = op.get_all_host_id_from_response(res2)
-                session['hosts'] = [session['clusters'][i], host_temp]
-                for j in range(len(session['hosts'][1])):
-                    res3 = op.get_disks_by_host_id(url,port,key,session['hosts'][1][j])
+                session['hosts'].append([session['clusters'][i], host_temp])
+                for j in range(len(session['hosts'][i][1])):
+                    res3 = op.get_disks_by_host_id(url,port,key,session['hosts'][i][1][j])
                     disk_temp=op.get_all_disk_id_from_response(res3)
-                    session['disks'] = [session['clusters'][i], session['hosts'][1][j], disk_temp]
+                    all_info = res3.json()
+                    session['disks'].append([session['clusters'][i], session['hosts'][i][1][j], disk_temp, all_info])
+                    for disk in disk_temp:
+                        session['all_disks'].append(disk)
+                        tmp = []
+                        [tmp.append(x) for x in session['all_disks'] if x not in tmp]
+                        session['all_disks'] = tmp
                               # Print Operation
-
+    
     except requests.exceptions.MissingSchema:       # Invalid URL will throw this exception
         flash("invalid url", type="error")               # Print Error Message
+        
+    if request.method == 'POST':
+        operation = request.form.get("submit_button")
+        machines = []
+        disk_list = []
+        for disks in session["disks"]:
+            for element in disks[2]:
+                machines.append(request.form.get(element))
+        #flash(session['all_disks'])
+        n = 0
+        for machine in machines:
+            
+            if machine is not None:
+                disk_list.append(session['all_disks'][n])
+            n+=1
+
+        for item in disk_list:
+            if operation == '1':
+                flash('Tags DATADISK, METADATADISK given to disk: ' + str(item))
+            elif operation == 2:
+                pass
+            elif operation ==3:
+                pass
+            elif operation ==4:
+                pass
+
+
+        
+                
+        #flash(operation)
+        #flash(disk_list)
+
 
     return render_template('manage.html', user = current_user, disks = session['disks'], hosts = session['hosts'])
+
+
